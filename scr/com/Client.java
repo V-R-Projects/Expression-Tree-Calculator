807073dcc18ff473c2aea93a2fdf940bcaf09617
valesskablanco <valesskablanco2@gmail.com>
2021-10-26 14:39:05 -0600

    Comunicación por Sockets

--- scr/com/Client.java
+++ scr/com/Client.java
@@ -1,4 +1,29 @@
 package com;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
+
+    private static final String ip = "127.0.0.1";
+    private static final int puerto = 9090;
+    //public static GUI frame;
+
+    public static void main(String[] args) throws IOException {
+
+        //frame = new GUI();
+
+        Socket socket = new Socket(ip, puerto);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+
+        //socket.close();
+
+    }
 }
--- scr/com/ClientHandler.java
+++ scr/com/ClientHandler.java
@@ -1,4 +1,49 @@
 package com;
 
-public class ClientHandler {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+
+    private final Socket client;
+    private final BufferedReader in;
+    private final PrintWriter out;
+
+    public ClientHandler(Socket clientSocket) throws IOException {
+
+        this.client = clientSocket;
+        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        out = new PrintWriter(client.getOutputStream(), true);
+    }
+
+    @Override
+    public void run() {
+
+        try {
+
+            while (true) {
+
+                String datos = in.readLine();
+            }
+
+        } catch (IOException e) {
+
+            System.err.println("Cliente Desconectado...");
+        } finally {
+
+            try {
+
+                in.close();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+            out.close();
+        }
+
+    }
 }
