33dc64b85ba657a6382a012aafd23f6b6b652621
Ramsensei <ramsensei@estudiantec.cr>
2021-11-05 11:28:08 -0600

    calculadora v1

--- scr/interfaz/Button.java
+++ scr/interfaz/Button.java
@@ -1,10 +1,12 @@
 package interfaz;
 
+import tree.ExpressionTree;
+import tree.Node;
+
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
+import javax.swing.*;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 
@@ -17,14 +19,35 @@ class Button implements ActionListener {
         this.interfaz = clientInterface;
         this.opText = text;
         this.button = new JButton(text);
-        this.button.setSize(40 + plus, 40);
+        this.button.setSize(50 + plus, 40);
         this.button.setLocation(x, y);
         this.button.addActionListener(this);
         this.interfaz.panel.add(this.button);
     }
 
     public void actionPerformed(ActionEvent e) {
+        click();
+    }
 
-
+    public void click(){
+        String text = this.interfaz.operation.getText();
+        switch (this.opText) {
+            case "AC":
+                this.interfaz.operation.setText("");
+                break;
+            case "DEL":
+                if (text.length() > 0)
+                    this.interfaz.operation.setText(text.substring(0, text.length() - 1));
+                break;
+            case "=":
+                ExpressionTree tree = new ExpressionTree();
+                String Infix = text + "=";
+                Node root = tree.constructTreeInfix(Infix);
+                this.interfaz.operation.setText(Float.toString(tree.Evaluate(root)));
+                break;
+            default:
+                this.interfaz.operation.setText(text + this.opText);
+                break;
+        }
     }
 }
--- scr/interfaz/GUI.java
+++ scr/interfaz/GUI.java
@@ -1,5 +1,8 @@
 package interfaz;
 
+import tree.ExpressionTree;
+import tree.Node;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -36,7 +39,7 @@ public class GUI extends JFrame implements KeyListener{
 
         this.setTitle("Expression Tree Calculator");
         this.setVisible(true);
-        this.setSize(300, 300);
+        this.setSize(350, 300);
 
         this.panel = new JPanel();
         this.getContentPane().add(this.panel);
@@ -45,30 +48,31 @@ public class GUI extends JFrame implements KeyListener{
         this.setResizable(false);
 
         this.operation = new JTextField(20);
-        this.operation.setSize(270, 40);
+        this.operation.setSize(310, 40);
         this.operation.setLocation(10, 10);
         this.operation.addKeyListener(this);
         this.panel.add(this.operation);
+
         this.key1 = new Button(this, "1", 10, 60, 0);
-        this.key2 = new Button(this, "2", 60, 60, 0);
-        this.key3 = new Button(this, "3", 110, 60, 0);
+        this.key2 = new Button(this, "2", 70, 60, 0);
+        this.key3 = new Button(this, "3", 130, 60, 0);
         this.key4 = new Button(this, "4", 10, 110, 0);
-        this.key5 = new Button(this, "5", 60, 110, 0);
-        this.key6 = new Button(this, "6", 110, 110, 0);
+        this.key5 = new Button(this, "5", 70, 110, 0);
+        this.key6 = new Button(this, "6", 130, 110, 0);
         this.key7 = new Button(this, "7", 10, 160, 0);
-        this.key8 = new Button(this, "8", 60, 160, 0);
-        this.key9 = new Button(this, "9", 110, 160, 0);
+        this.key8 = new Button(this, "8", 70, 160, 0);
+        this.key9 = new Button(this, "9", 130, 160, 0);
         this.keyParent1 = new Button(this, "(", 10, 210, 0);
-        this.key0 = new Button(this, "0", 60, 210, 0);
-        this.keyParent2 = new Button(this, ")", 110, 210, 0);
-        this.keySum = new Button(this, "+", 160, 60, 1);
-        this.keyMin = new Button(this, "-", 160, 110, 0);
-        this.keyMult = new Button(this, "*", 160, 160, 0);
-        this.keyDiv = new Button(this, "/", 160, 210, 0);
-        this.keyPer = new Button(this, "%", 210, 60, 20);
-        this.keyDelLast = new Button(this, "DEL", 210, 110, 20);
-        this.keyDelAll = new Button(this, "AC", 210, 160, 20);
-        this.keyEq = new Button(this, "=", 210, 210, 20);
+        this.key0 = new Button(this, "0", 70, 210, 0);
+        this.keyParent2 = new Button(this, ")", 130, 210, 0);
+        this.keySum = new Button(this, "+", 190, 60, 1);
+        this.keyMin = new Button(this, "-", 190, 110, 0);
+        this.keyMult = new Button(this, "*", 190, 160, 0);
+        this.keyDiv = new Button(this, "/", 190, 210, 0);
+        this.keyPer = new Button(this, "^", 250, 60, 20);
+        this.keyDelLast = new Button(this, "DEL", 250, 110, 20);
+        this.keyDelAll = new Button(this, "AC", 250, 160, 20);
+        this.keyEq = new Button(this, "=", 250, 210, 20);
 
         this.panel.repaint();
         this.setDefaultCloseOperation(3);
@@ -76,8 +80,18 @@ public class GUI extends JFrame implements KeyListener{
 
     @Override
     public void keyTyped(KeyEvent e) {
-
+        if (!Character.isDigit(e.getKeyChar()) && !"/*+-^=".contains(Character.toString(e.getKeyChar()))) {
+            e.consume();
+        }
+        else if(e.getKeyChar() == '='){
+            ExpressionTree tree = new ExpressionTree();
+            String Infix = this.operation.getText();
+            Node root = tree.constructTreeInfix(Infix);
+            this.operation.setText(Float.toString(tree.Evaluate(root)));
+            e.consume();
+        }
     }
+
 
     @Override
     public void keyPressed(KeyEvent e) {
